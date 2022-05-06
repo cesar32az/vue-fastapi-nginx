4c321df1132110a8ac73dae98c60b548c7276fac
Julio Rodriguez <julio.rodriguez@ubiquo.io>
2022-05-06 09:36:44 -0600

    proyecto inicial, api sencilla y consumo desde vuejs

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed, Ref, ref } from "@vue/reactivity";
+import axios from "axios";
+
+interface Data {
+  message: string;
+}
+
+let result: Ref<string> = ref("Hi")
+
+const getData = async () => {
+  const response = await axios.get("http://localhost:8000/test")
+  const data: Data = response.data
+  result.value = data.message
+  console.log(result.value)
+}
+const message = computed(() => result.value)
+</script>
+
+<template>
+  <button @click="getData">
+    GET asdf
+  </button>
+  <div ref="message">
+    The message is: {{ message }}
+  </div>
+</template>
